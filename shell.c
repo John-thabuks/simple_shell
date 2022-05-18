7b37efbb807c2f223ece089b98ae941b53f7c2a2
Abdifatah Nur <deenynur21@gmail.com>
2022-05-19 01:06:05 +0300

    shell2

--- shell.c
+++ shell.c
@@ -5,34 +5,34 @@
 
 /**
  * main - shell
- * 
+ *
  * Return: 0
- * 
+ *
  */
 
 int main(void)
 {
-    char *args[] = {NULL, NULL};
-    char *command = NULL;
+	char *args[] = {NULL, NULL};
+	char *command = NULL;
 
-    ssize_t n_bytes = 0;
-    size_t command_len = 0;
+	ssize_t n_bytes = 0;
+	size_t command_len = 0;
 
-    while(1)
-    {
-        printf("#cisfun$ ");
-        n_bytes = getline(&command, &command_len, stdin);
-        command[n_bytes - 1] = '\0';
-        args[0] = command;
+	while (1)
+	{
+		printf("#cisfun$ ");
+		n_bytes = getline(&command, &command_len, stdin);
+		command[n_bytes - 1] = '\0';
 
-        if (fork() == 0)
-        {
-            execve(*args, args, NULL);
-            dprintf(STDERR_FILENO, "%s: Command not found.\n", *args);
-            exit(1);
-        }
-        else wait(NULL);
-    }
+		if (fork() == 0)
+		{
+			execve(*args, args, NULL);
+			dprintf(STDERR_FILENO, "%s: Command not found.\n", *args);
+			exit(1);
+		}
+		else
+			wait(NULL);
+	}
 
-    return (0);
+	return (0);
 }
